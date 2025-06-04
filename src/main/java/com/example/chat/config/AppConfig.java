9ca9e9e8693e42c7dc9a2836af789e389a6aa680
Vo Van Khanh <khanh606m@gmail.com>
2025-06-04 21:30:33 +0700

    To 2.0

--- src/main/java/com/example/chat/config/AppConfig.java
+++ src/main/java/com/example/chat/config/AppConfig.java
@@ -3,12 +3,14 @@ package com.example.chat.config;
 import com.example.chat.TCP.Server.TCPServer;
 import com.example.chat.websocket.ChatWebSocketHandler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@ComponentScan("com.example.chat")
 @EnableWebSocket
 public class AppConfig implements WebSocketConfigurer {
 
